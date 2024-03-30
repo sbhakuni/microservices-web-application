7618790e5cc48847524601d5cb4d77c17da5b73d
sbhakuni <bhakunishaantanu@gmail.com>
2024-03-29 18:23:42 -0700

    bugfix-model and view

--- admin/products/views.py
+++ admin/products/views.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Product, User
-# from .producer import publish
 from .serializers import ProductSerializer
 import random
 
@@ -18,7 +17,6 @@ class ProductViewSet(viewsets.ViewSet):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        # publish('product_created', serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
@@ -31,13 +29,11 @@ class ProductViewSet(viewsets.ViewSet):
         serializer = ProductSerializer(instance=product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        # publish('product_updated', serializer.data)
         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
     def destroy(self, request, pk=None):
         product = Product.objects.get(id=pk)
         product.delete()
-        # publish('product_deleted', pk)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
